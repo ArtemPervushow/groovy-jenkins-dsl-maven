c615616da3d7c192f30a5fc674ec6458cce24761
ArtemPervushow <drperv@yandex.ru>
2019-07-23 14:51:57 +0300

    Add files via upload

---
+++ src/main/groovy/jobs_maven.groovy
@@ -0,0 +1,45 @@
+import javaposse.jobdsl.dsl.*
+
+class MavenJobBuilder {
+    String jobName
+    String description
+    String gitUrl
+    String branchName
+    String credentialsId
+    Integer numToKeep
+    Integer daysToKeep
+
+    Job build(DslFactory dslFactory){
+        dslFactory.mavenJob(jobName){
+            description(this.description)
+            logRotator{
+                numToKeep = this.numToKeep
+                daysToKeep = this.daysToKeep
+            }
+            scm {
+                git {
+                    remote {
+                        name('origin')
+                        url(this.gitUrl)
+                        credentials(this.credentialsId)
+                    }
+                    branch(this.branchName)
+                }
+            }
+            goals('clean package')
+        }
+    }
+}
+
+folder("git-maven-folder"){
+    description 'This is folder for maven build'
+}
+
+def builder = new MavenJobBuilder(  jobName:"Maven Job", description:"This is my first maven job",
+                                    gitUrl:"https://github.com/ArtemPervushow/groovy-jenkins-dsl-maven.git",
+                                    branchName:"master",
+                                    credentialsId:"cacc3e70-7103-4613-b74b-eaa04c825483",
+                                    numToKeep:10,
+                                    daysToKeep:20)
+
+builder.build(this)
